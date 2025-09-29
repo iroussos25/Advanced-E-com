ed2997ffa7d781fdf4e0fd8155aa84d0b521aef2
Yiannis <ioannis.roussos@hotmail.com>
2025-09-29 17:17:55 +0300

    updating github

--- books.js
+++ books.js
@@ -1,16 +1,16 @@
 function renderBooks() {
-const booksWrapper = document.querySelector('.books');
+const booksWrapper = document.querySelector('.book');
 
 const books = getBooks();
-console.log(books)
- 
-  booksWrapper.innerHTML = 
-`<div class="book">
+
+
+const booksHtml = books.map(book =>{
+return `<div class="book">
 <figure class="book__img--wrapper">
-  <img class="book__img" src="${books[0].url}" alt="">
+  <img class="book__img" src="${book.url}" alt="">
 </figure>
 <div class="book__title">
-${books[0].title}
+${book.title}
 </div>
 <div class="book__ratings">
   <i class="fas fa-star"></i>
@@ -20,9 +20,13 @@ ${books[0].title}
   <i class="fas fa-star-half-alt"></i>
 </div>
 <div class="book__price">
-  <span class="book__price--normal">$59.95</span> $14.95
+  <span class="book__price--normal">$${books[0].originalPrice}</span> $${books[0].salePrice}
 </div>
 </div>`
+} ) 
+  booksWrapper.innerHTML = booksHtml;
+  console.log(booksHtml)
+
 
 }
 
